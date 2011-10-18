4e35c0c9d17f9cc7ebc93c70dffcf27e578ca566
Kevin Peterson <kevin.peterson@mayo.edu>
2011-10-18 06:45:48 -0500

    Added tests.

--- cts2-framework/filter/src/test/groovy/edu/mayo/cts2/framework/filter/directory/AbstractRemovingDirectoryBuilderTest.groovy
+++ cts2-framework/filter/src/test/groovy/edu/mayo/cts2/framework/filter/directory/AbstractRemovingDirectoryBuilderTest.groovy
@@ -1,44 +1,53 @@
 package edu.mayo.cts2.framework.filter.directory;
 
-import org.junit.Test;
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+import edu.mayo.cts2.framework.filter.match.AttributeResolver
+import edu.mayo.cts2.framework.filter.match.ResolvableModelAttributeReference
+import edu.mayo.cts2.framework.model.core.FilterComponent
+import edu.mayo.cts2.framework.model.core.URIAndEntityName
+import edu.mayo.cts2.framework.model.core.types.TargetReferenceType
 
 class AbstractRemovingDirectoryBuilderTest {
-	
+
 	@Test
 	void testGetCandidateText(){
-		/* TODO: Make this pass on the CI groovy environment...
-		 
+
 		def builder = new TestAbstractRemovingDirectoryBuilder()
-		
+
 		def filter = new FilterComponent(matchValue:"test")
 		def name = new URIAndEntityName(name:"string", uri:"http://string/")
 		filter.setReferenceTarget(name);
-		
-		def attributeResolver = [resolveAttribute: { obj -> return [obj] }]  as AttributeResolver
+
+		def attributeResolver = [resolveAttribute: {
+				obj -> return [obj]
+			}]  as AttributeResolver
 		ResolvableModelAttributeReference modelRef = new ResolvableModelAttributeReference(attributeResolver)
 		modelRef.setContent("string")
 		modelRef.setUri("http://string/")
 		builder.addSupportedModelAttributeReference(modelRef);
-		
+
 		def result = builder.getCandidateText(filter, TargetReferenceType.ATTRIBUTE, "one")
-		
-		assertEquals( ["one"], result )
-		*/
-	}
-	
-	/*
-	class TestAbstractRemovingDirectoryBuilder extends AbstractRemovingDirectoryBuilder<String,String> {
 
-		public TestAbstractRemovingDirectoryBuilder() {
-			super(Arrays.asList("one", "two"));
-		}
-
-		@Override
-		protected List<String> transformResults(List results) {
-			// TODO Auto-generated method stub
-			return null;
-		}
+		def expected = ["one"]
 		
+		assertEquals expected, result
 	}
-	*/
 }
+
+class TestAbstractRemovingDirectoryBuilder extends AbstractRemovingDirectoryBuilder<String,String> {
+
+	public TestAbstractRemovingDirectoryBuilder() {
+		super(Arrays.asList("one", "two"));
+	}
+
+	@Override
+	protected List<String> transformResults(List results) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+
