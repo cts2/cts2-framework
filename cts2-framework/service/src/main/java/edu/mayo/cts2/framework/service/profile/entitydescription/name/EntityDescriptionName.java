c1f7732247f8691aff37a543d83aec47ac40bb43
Kevin Peterson <kevin.peterson@mayo.edu>
2011-10-18 11:42:58 -0500

    Add codeSystemVersionName to attribute from constructor.

--- cts2-framework/service/src/main/java/edu/mayo/cts2/framework/service/profile/entitydescription/name/EntityDescriptionName.java
+++ cts2-framework/service/src/main/java/edu/mayo/cts2/framework/service/profile/entitydescription/name/EntityDescriptionName.java
@@ -37,7 +37,7 @@ public class EntityDescriptionName extends ResourceIdentifier<ScopedEntityName> 
 	
 	public EntityDescriptionName(ScopedEntityName name, String codeSystemVersionName) {
 		super(name);
-		
+		this.codeSystemVersionName = codeSystemVersionName;
 	}
 
 	public String getCodeSystemVersionName() {
