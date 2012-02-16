2a089da9681e7884096dd6b034103b2fb5ce6c47
suesse <dalesuesse@gmail.com>
2012-02-16 14:48:05 -0600

    Implements CodeSystemCatalogReadServicesEndpoint for issue: https://github.com/cts2/cts2-framework/issues/8

--- webapp-rest/src/main/java/edu/mayo/cts2/framework/webapp/soap/endpoint/codesystem/CodeSystemCatalogReadServicesEndpoint.java
+++ webapp-rest/src/main/java/edu/mayo/cts2/framework/webapp/soap/endpoint/codesystem/CodeSystemCatalogReadServicesEndpoint.java
@@ -1,5 +1,9 @@
 package edu.mayo.cts2.framework.webapp.soap.endpoint.codesystem;
 
+import edu.mayo.cts2.framework.model.service.core.DocumentedNamespaceReference;
+import edu.mayo.cts2.framework.model.wsdl.baseservice.*;
+import edu.mayo.cts2.framework.model.wsdl.codesystemread.Exists;
+import edu.mayo.cts2.framework.model.wsdl.codesystemread.ExistsResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -30,7 +34,89 @@ public class CodeSystemCatalogReadServicesEndpoint extends AbstractReadServiceEn
 		readResponse.setReturn(entry);
 		
 		return readResponse;
-		
 	}
+
+  @PayloadRoot(localPart = "exists", namespace = "http://schema.omg.org/spec/CTS2/1.0/wsdl/CodeSystemCatalogReadServices")
+  @ResponsePayload
+  public ExistsResponse exists(@RequestPayload Exists request) {
+    boolean exists = this.codeSystemReadService.exists(request.getCodeSystemId(), request.getContext());
+
+    ExistsResponse existsResponse = new ExistsResponse();
+    existsResponse.setReturn(exists);
+
+    return existsResponse;
+  }
+
+  @PayloadRoot(localPart = "getDefaultFormat", namespace = "http://schema.omg.org/spec/CTS2/1.0/wsdl/BaseServiceTypes")
+  @ResponsePayload
+  public GetDefaultFormatResponse getDefaultFormat(@RequestPayload GetDefaultFormat request) {
+    GetDefaultFormatResponse response = new GetDefaultFormatResponse();
+    response.setReturn(this.codeSystemReadService.getDefaultFormat());
+    return response;
+  }
+
+  @PayloadRoot(localPart = "getImplementationType", namespace = "http://schema.omg.org/spec/CTS2/1.0/wsdl/BaseServiceTypes")
+  @ResponsePayload
+  public GetImplementationTypeResponse getImplementationType(@RequestPayload GetImplementationType request) {
+    GetImplementationTypeResponse response = new GetImplementationTypeResponse();
+//    response.setReturn(ImplementationProfile.IP_SOAP);
+    return response;
+  }
+
+  @PayloadRoot(localPart = "getKnownNamespace", namespace = "http://schema.omg.org/spec/CTS2/1.0/wsdl/BaseServiceTypes")
+  @ResponsePayload
+  public GetKnownNamespaceResponse getKnownNamespace(@RequestPayload GetKnownNamespace request) {
+    GetKnownNamespaceResponse response = new GetKnownNamespaceResponse();
+    response.setReturn(this.codeSystemReadService.getKnownNamespaceList());
+    return response;
+  }
+
+  @PayloadRoot(localPart = "getServiceDescription", namespace = "http://schema.omg.org/spec/CTS2/1.0/wsdl/BaseServiceTypes")
+  @ResponsePayload
+  public GetServiceDescriptionResponse getServiceDescription(@RequestPayload GetServiceDescription request) {
+    GetServiceDescriptionResponse response = new GetServiceDescriptionResponse();
+    response.setReturn(this.codeSystemReadService.getServiceDescription());
+    return response;
+  }
+
+  @PayloadRoot(localPart = "getServiceName", namespace = "http://schema.omg.org/spec/CTS2/1.0/wsdl/BaseServiceTypes")
+  @ResponsePayload
+  public GetServiceNameResponse getServiceName(@RequestPayload GetServiceName request) {
+    GetServiceNameResponse response = new GetServiceNameResponse();
+    response.setReturn(this.codeSystemReadService.getServiceName());
+    return response;
+  }
+
+  @PayloadRoot(localPart = "getServiceProvider", namespace = "http://schema.omg.org/spec/CTS2/1.0/wsdl/BaseServiceTypes")
+  @ResponsePayload
+  public GetServiceProviderResponse getServiceProvider(@RequestPayload GetServiceProvider request) {
+    GetServiceProviderResponse response = new GetServiceProviderResponse();
+    response.setReturn(this.codeSystemReadService.getServiceProvider());
+    return response;
+  }
+
+  @PayloadRoot(localPart = "getServiceVersion", namespace = "http://schema.omg.org/spec/CTS2/1.0/wsdl/BaseServiceTypes")
+  @ResponsePayload
+  public GetServiceVersionResponse getServiceVersion(@RequestPayload GetServiceVersion request) {
+    GetServiceVersionResponse response = new GetServiceVersionResponse();
+    response.setReturn(this.codeSystemReadService.getServiceVersion());
+    return response;
+  }
+
+  @PayloadRoot(localPart = "getSupportedFormat", namespace = "http://schema.omg.org/spec/CTS2/1.0/wsdl/BaseServiceTypes")
+  @ResponsePayload
+  public GetSupportedFormatResponse getSupportedFormat(@RequestPayload GetSupportedFormat request) {
+    GetSupportedFormatResponse response = new GetSupportedFormatResponse();
+    response.setReturn(this.codeSystemReadService.getSupportedFormatList());
+    return response;
+  }
+
+  @PayloadRoot(localPart = "getSupportedProfile", namespace = "http://schema.omg.org/spec/CTS2/1.0/wsdl/BaseServiceTypes")
+  @ResponsePayload
+  public GetSupportedProfileResponse getSupportedProfile(@RequestPayload GetSupportedProfile request) {
+    GetSupportedProfileResponse response = new GetSupportedProfileResponse();
+//    response.setReturn(this.baseService.getSupportedProfile());
+    return response;
+  }
 
 }
--- webapp-rest/src/test/java/edu/mayo/cts2/framework/webapp/soap/endpoint/codesystem/CodeSystemCatalogReadServicesEndpointTestIT.java
+++ webapp-rest/src/test/java/edu/mayo/cts2/framework/webapp/soap/endpoint/codesystem/CodeSystemCatalogReadServicesEndpointTestIT.java
@@ -1,95 +1,172 @@
 package edu.mayo.cts2.framework.webapp.soap.endpoint.codesystem;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.List;
-
-import org.junit.Test;
-
 import edu.mayo.cts2.framework.model.codesystem.CodeSystemCatalogEntry;
 import edu.mayo.cts2.framework.model.command.ResolvedReadContext;
 import edu.mayo.cts2.framework.model.core.FormatReference;
 import edu.mayo.cts2.framework.model.core.NamespaceReference;
 import edu.mayo.cts2.framework.model.core.OpaqueData;
 import edu.mayo.cts2.framework.model.core.SourceReference;
+import edu.mayo.cts2.framework.model.service.core.DocumentedNamespaceReference;
 import edu.mayo.cts2.framework.model.service.core.NameOrURI;
 import edu.mayo.cts2.framework.model.service.core.ReadContext;
 import edu.mayo.cts2.framework.model.util.ModelUtils;
+import edu.mayo.cts2.framework.model.wsdl.baseservice.*;
+import edu.mayo.cts2.framework.model.wsdl.codesystemread.Exists;
+import edu.mayo.cts2.framework.model.wsdl.codesystemread.ExistsResponse;
 import edu.mayo.cts2.framework.model.wsdl.codesystemread.Read;
 import edu.mayo.cts2.framework.model.wsdl.codesystemread.ReadResponse;
 import edu.mayo.cts2.framework.service.profile.codesystem.CodeSystemReadService;
 import edu.mayo.cts2.framework.webapp.service.MockServiceProvider;
 import edu.mayo.cts2.framework.webapp.soap.endpoint.SoapEndpointTestBase;
+import org.apache.commons.lang.ArrayUtils;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CodeSystemCatalogReadServicesEndpointTestIT extends SoapEndpointTestBase {
 
-	String uri = "http://localhost:8081/webapp-rest/soap/service/CodeSystemCatalogReadService";
-	
-	@Test
-	public void TestRead() throws Exception{
-	
-		MockServiceProvider.cts2Service = new CodeSystemReadService(){
+  String uri = "http://localhost:8081/webapp-rest/soap/service/CodeSystemCatalogReadService";
 
-			@Override
-			public CodeSystemCatalogEntry read(NameOrURI identifier,
-					ResolvedReadContext readContext) {
-				CodeSystemCatalogEntry entry = new CodeSystemCatalogEntry();
-				entry.setCodeSystemName("test");
-				entry.setAbout("testAbout");
-				return entry;
-			}
+  @Test
+  public void TestRead() throws Exception {
+    MockServiceProvider.cts2Service = new MockCodeSystemReadService();
+    Read readRequest = new Read();
+    readRequest.setCodeSystemId(ModelUtils.nameOrUriFromName("test"));
+    ReadResponse response = (ReadResponse) this.doSoapCall(uri, readRequest);
+    assertEquals(response.getReturn().getCodeSystemName(), "test");
+  }
 
-			@Override
-			public boolean exists(NameOrURI identifier, ReadContext readContext) {
-				throw new UnsupportedOperationException();
-			}
+  @Test
+  public void TestExists() throws Exception {
+    MockServiceProvider.cts2Service = new MockCodeSystemReadService();
+    Exists existsRequest = new Exists();
+    existsRequest.setCodeSystemId(ModelUtils.nameOrUriFromName("test"));
+    ExistsResponse response = (ExistsResponse) this.doSoapCall(uri, existsRequest);
+    assertTrue(response.getReturn());
+  }
 
-			@Override
-			public String getServiceName() {
-				throw new UnsupportedOperationException();
-			}
+  @Test
+  public void TestGetServiceName() throws Exception {
+    MockServiceProvider.cts2Service = new MockCodeSystemReadService();
+    GetServiceName request = new GetServiceName();
+    GetServiceNameResponse response = (GetServiceNameResponse) this.doSoapCall(uri, request);
+    assertEquals(CodeSystemCatalogReadServicesEndpointTestIT.class.getName(), response.getReturn());
+  }
 
-			@Override
-			public OpaqueData getServiceDescription() {
-				throw new UnsupportedOperationException();
-			}
+  @Test
+  public void TestGetServiceVersion() throws Exception {
+    MockServiceProvider.cts2Service = new MockCodeSystemReadService();
+    GetServiceVersion request = new GetServiceVersion();
+    GetServiceVersionResponse response = (GetServiceVersionResponse) this.doSoapCall(uri, request);
+    assertEquals("1.23.4", response.getReturn());
+  }
 
-			@Override
-			public String getServiceVersion() {
-				throw new UnsupportedOperationException();
-			}
+  @Test
+  public void TestGetDefaultFormat() throws Exception {
+    MockServiceProvider.cts2Service = new MockCodeSystemReadService();
+    GetDefaultFormat request = new GetDefaultFormat();
+    GetDefaultFormatResponse response = (GetDefaultFormatResponse) this.doSoapCall(uri, request);
+    assertEquals(new FormatReference("testRef"), response.getReturn());
+  }
 
-			@Override
-			public SourceReference getServiceProvider() {
-				throw new UnsupportedOperationException();
-			}
+  @Test
+  public void TestGetServiceProvider() throws Exception {
+    MockServiceProvider.cts2Service = new MockCodeSystemReadService();
+    GetServiceProvider request = new GetServiceProvider();
+    GetServiceProviderResponse response = (GetServiceProviderResponse) this.doSoapCall(uri, request);
+    assertEquals(new SourceReference("test ref"), response.getReturn());
+  }
 
-			@Override
-			public List<FormatReference> getSupportedFormatList() {
-				throw new UnsupportedOperationException();
-			}
+  @Test
+  public void TestGetServiceDescription() throws Exception {
+    MockServiceProvider.cts2Service = new MockCodeSystemReadService();
+    GetServiceDescription request = new GetServiceDescription();
+    GetServiceDescriptionResponse response = (GetServiceDescriptionResponse) this.doSoapCall(uri, request);
+    OpaqueData expected = new OpaqueData();
+    expected.setValue(ModelUtils.toTsAnyType("test desc"));
+    assertEquals(expected, response.getReturn());
+  }
 
-			@Override
-			public FormatReference getDefaultFormat() {
-				throw new UnsupportedOperationException();
-			}
+  @Test
+  public void TestGetKnownNamespaceList() throws Exception {
+    MockServiceProvider.cts2Service = new MockCodeSystemReadService();
+    GetKnownNamespace request = new GetKnownNamespace();
+    GetKnownNamespaceResponse response = (GetKnownNamespaceResponse) this.doSoapCall(uri, request);
+    NamespaceReference[] namespaceReferences = response.getReturn();
+    assertEquals(3, namespaceReferences.length);
+    assertTrue(ArrayUtils.contains(namespaceReferences, new DocumentedNamespaceReference("ns1")));
+    assertTrue(ArrayUtils.contains(namespaceReferences, new DocumentedNamespaceReference("ns2")));
+    assertTrue(ArrayUtils.contains(namespaceReferences, new DocumentedNamespaceReference("ns3")));
+  }
 
-			@Override
-			public List<NamespaceReference> getKnownNamespaceList() {
-				throw new UnsupportedOperationException();
-			}
-			
-		};
-		
-		Read readRequest = new Read();
-		readRequest.setCodeSystemId(ModelUtils.nameOrUriFromName("test"));
-		
-		ReadResponse response = (ReadResponse) this.doSoapCall(uri, readRequest);
-		
-		assertEquals(response.getReturn().getCodeSystemName(), "test");
-		
-	}
+  @Test
+  public void TestGetSupportedFormatList() throws Exception {
+    MockServiceProvider.cts2Service = new MockCodeSystemReadService();
+    GetSupportedFormat request = new GetSupportedFormat();
+    GetSupportedFormatResponse response = (GetSupportedFormatResponse) this.doSoapCall(uri, request);
+    FormatReference[] formatReferences = response.getReturn();
+    assertEquals(3, formatReferences.length);
+    assertTrue(ArrayUtils.contains(formatReferences, new FormatReference("fr1")));
+    assertTrue(ArrayUtils.contains(formatReferences, new FormatReference("fr2")));
+    assertTrue(ArrayUtils.contains(formatReferences, new FormatReference("fr3")));
+  }
 
+  protected class MockCodeSystemReadService implements CodeSystemReadService {
+
+    public CodeSystemCatalogEntry read(NameOrURI identifier,
+                                       ResolvedReadContext readContext) {
+      CodeSystemCatalogEntry entry = new CodeSystemCatalogEntry();
+      entry.setCodeSystemName("test");
+      entry.setAbout("testAbout");
+      return entry;
+    }
+
+    public boolean exists(NameOrURI identifier, ReadContext readContext) {
+      return identifier.getName().equals("test");
+    }
+
+    public String getServiceName() {
+      return CodeSystemCatalogReadServicesEndpointTestIT.class.getName();
+    }
+
+    public OpaqueData getServiceDescription() {
+      OpaqueData opaqueData = new OpaqueData();
+      opaqueData.setValue(ModelUtils.toTsAnyType("test desc"));
+      return opaqueData;
+    }
+
+    public String getServiceVersion() {
+      return "1.23.4";
+    }
+
+    public SourceReference getServiceProvider() {
+      return new SourceReference("test ref");
+    }
+
+    public List<FormatReference> getSupportedFormatList() {
+      List<FormatReference> formatReferences = new ArrayList<FormatReference>(3);
+      formatReferences.add(new FormatReference("fr1"));
+      formatReferences.add(new FormatReference("fr2"));
+      formatReferences.add(new FormatReference("fr3"));
+      return formatReferences;
+    }
+
+    public FormatReference getDefaultFormat() {
+      return new FormatReference("testRef");
+    }
+
+    public List<DocumentedNamespaceReference> getKnownNamespaceList() {
+      List<DocumentedNamespaceReference> namespaceReferences = new ArrayList<DocumentedNamespaceReference>(3);
+      namespaceReferences.add(new DocumentedNamespaceReference("ns1"));
+      namespaceReferences.add(new DocumentedNamespaceReference("ns2"));
+      namespaceReferences.add(new DocumentedNamespaceReference("ns3"));
+      return namespaceReferences;
+    }
+  }
 
 }
 
