0bab193385a1d1cda3f19a328eebe7aaa4323a79
Kevin Peterson <kevin.peterson@mayo.edu>
2013-05-23 13:06:10 -0500

    Fixed a TODO for Active Options.

--- webapp/src/main/java/edu/mayo/cts2/framework/webapp/rest/resolver/ReadContextResolver.java
+++ webapp/src/main/java/edu/mayo/cts2/framework/webapp/rest/resolver/ReadContextResolver.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright: (c) 2004-2013 Mayo Foundation for Medical Education and 
+ * Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+ * triple-shield Mayo logo are trademarks and service marks of MFMER.
+ *
+ * Except as contained in the copyright notice above, or as used to identify 
+ * MFMER as the author of this software, the trade names, trademarks, service
+ * marks, or product names of the copyright holder shall not be used in
+ * advertising, promotion or otherwise in connection with this software without
+ * prior written authorization of the copyright holder.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.mayo.cts2.framework.webapp.rest.resolver;
 
 import org.springframework.stereotype.Component;
@@ -5,9 +28,20 @@ import org.springframework.stereotype.Component;
 import edu.mayo.cts2.framework.model.command.ResolvedReadContext;
 import edu.mayo.cts2.framework.webapp.rest.command.RestReadContext;
 
+/**
+ * Turn a {@link RestReadContext} into a {@link ResolvedReadContext}.
+ *
+ * @author <a href="mailto:kevin.peterson@mayo.edu">Kevin Peterson</a>
+ */
 @Component
 public class ReadContextResolver {
 
+	/**
+	 * Resolve rest read context.
+	 *
+	 * @param context the context
+	 * @return the resolved read context
+	 */
 	public ResolvedReadContext resolveRestReadContext(RestReadContext context){
 		if(context == null){
 			return null;
@@ -15,7 +49,10 @@ public class ReadContextResolver {
 		
 		ResolvedReadContext resolvedContext = new ResolvedReadContext();
 		resolvedContext.setChangeSetContextUri(context.getChangesetcontext());
-		//TODO: Finish this method
+		resolvedContext.setActive(context.getActive());
+		resolvedContext.setReferenceTime(context.getReferencetime());
+		resolvedContext.setLanguageReference(context.getLanguage());
+		
 		return resolvedContext;
 	}
 }
