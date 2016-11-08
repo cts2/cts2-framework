c3c4050d4982fa2d989e2624b0f18e834afad0d4
Scott Bauer <bauer.scott@mayo.edu>
2016-11-08 13:22:09 -0600

    CTS2FW-23.  Removing soap Unit tests

--- cts2-webapp/src/test/java/edu/mayo/cts2/framework/webapp/soap/endpoint/statement/StatementReadServicesEndpointTestIT.java
+++
@@ -1,6 +0,0 @@
-package edu.mayo.cts2.framework.webapp.soap.endpoint.statement;
-
-import edu.mayo.cts2.framework.webapp.soap.endpoint.SoapEndpointTestBase;
-
-public class StatementReadServicesEndpointTestIT extends SoapEndpointTestBase {
-}
